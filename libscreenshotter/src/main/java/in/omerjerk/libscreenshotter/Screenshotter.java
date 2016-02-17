1ae096916e60c246d70fa94f85d79a03f3b13a3e
Umair Khan <omerjerk@gmail.com>
2016-02-17 20:52:05 +0530

    Initial commit
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

---
+++ libscreenshotter/src/main/java/in/omerjerk/libscreenshotter/Screenshotter.java
@@ -0,0 +1,18 @@
+package in.omerjerk.libscreenshotter;
+
+import android.content.Intent;
+
+/**
+ * Created by omerjerk on 17/2/16.
+ */
+public class Screenshotter {
+
+    /**
+     * Takes the screenshot of whatever currently is on the default display.
+     * @param resultCode The result code returned by the request for accessing MediaProjection permission
+     * @param data The intent returned by the same request
+     */
+    public static void takeScreenshot(int resultCode, Intent data) {
+        
+    }
+}
