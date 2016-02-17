503e96756ea4599d5db21c64dbbabe8982537389
Umair Khan <omerjerk@gmail.com>
2016-02-17 21:31:48 +0530

    logic for taking the screenshot
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- libscreenshotter/src/main/java/in/omerjerk/libscreenshotter/Screenshotter.java
+++ libscreenshotter/src/main/java/in/omerjerk/libscreenshotter/Screenshotter.java
@@ -1,18 +1,64 @@
 package in.omerjerk.libscreenshotter;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.hardware.display.DisplayManager;
+import android.hardware.display.VirtualDisplay;
+import android.media.MediaCodec;
+import android.media.MediaCodecInfo;
+import android.media.MediaFormat;
+import android.media.projection.MediaProjection;
+import android.media.projection.MediaProjectionManager;
+import android.util.Log;
+import android.view.Surface;
+import android.view.TextureView;
+
+import java.io.IOException;
 
 /**
  * Created by omerjerk on 17/2/16.
  */
 public class Screenshotter {
 
+    private static final String TAG = "LibScreenshotter";
+
+    private VirtualDisplay virtualDisplay;
+    private Surface mSurface;
+
+    private int width;
+    private int height;
+
+    private TextureView textureView;
+
     /**
      * Takes the screenshot of whatever currently is on the default display.
      * @param resultCode The result code returned by the request for accessing MediaProjection permission
      * @param data The intent returned by the same request
      */
-    public static void takeScreenshot(int resultCode, Intent data) {
-        
+    public Bitmap takeScreenshot(Context context, int resultCode, Intent data, int width, int height)
+            throws IOException{
+        MediaProjectionManager mMediaProjectionManager = (MediaProjectionManager)
+                context.getSystemService(Context.MEDIA_PROJECTION_SERVICE);
+        MediaProjection mMediaProjection = mMediaProjectionManager.getMediaProjection(resultCode, data);
+
+        this.width = width;
+        this.height = height;
+
+        textureView = new TextureView(context);
+        mSurface = new Surface(textureView.getSurfaceTexture());
+
+        virtualDisplay = mMediaProjection.createVirtualDisplay("Remote Droid",
+                width, height, 50,
+                DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
+                mSurface, null, null);
+
+        try {
+            Thread.sleep(1000); //Let's wait for 1 sec for the virtual display to start properly
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return textureView.getBitmap(width, height);
     }
 }
